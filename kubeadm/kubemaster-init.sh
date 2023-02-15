58c8474ff984c341dcbc952b00fee4313fc4da59
manu <manu13@gmail.com>
2023-02-15 11:51:22 +0530

    imp files

--- kubeadm/kubemaster-init.sh
+++ kubeadm/kubemaster-init.sh
@@ -12,46 +12,6 @@ net.bridge.bridge-nf-call-iptables = 1
 EOF
 sudo sysctl --system
 
-## INSTALLING DOCKER ENGINE BASED ON OS
-
-yum --help &>> /dev/null
-if [ $? -eq 0 ]
-then
-  # (Install Docker CE)
-  ## Set up the repository
-  ### Install required packages
-  yum install -y yum-utils device-mapper-persistent-data lvm2
-  ## Add the Docker repository
-  yum-config-manager --add-repo \
-    https://download.docker.com/linux/centos/docker-ce.repo
-  # Install Docker CE
-  yum update -y && yum install -y \
-    containerd.io-1.2.13 \
-    docker-ce-19.03.11 \
-    docker-ce-cli-19.03.11
-  ## Create /etc/docker
-  mkdir /etc/docker
-  # Set up the Docker daemon
-  cat > /etc/docker/daemon.json <<EOF
-  {
-    "exec-opts": ["native.cgroupdriver=systemd"],
-    "log-driver": "json-file",
-    "log-opts": {
-      "max-size": "100m"
-    },
-    "storage-driver": "overlay2",
-    "storage-opts": [
-      "overlay2.override_kernel_check=true"
-    ]
-  }
-EOF
-  mkdir -p /etc/systemd/system/docker.service.d
-  # Restart Docker
-  systemctl daemon-reload
-  systemctl restart docker
-  sudo systemctl enable docker
-
-else
   # (Install Docker CE)
   ## Set up the repository:
   ### Install packages to allow apt to use a repository over HTTPS
@@ -91,32 +51,6 @@ fi
 sleep 30
 
 ## Installing kubeadm, kubelet and kubectl
-yum --help &>> /dev/null
-if [ $? -eq 0 ]
-then
-   cat <<EOF | sudo tee /etc/yum.repos.d/kubernetes.repo
-   [kubernetes]
-   name=Kubernetes
-   baseurl=https://packages.cloud.google.com/yum/repos/kubernetes-el7-\$basearch
-   enabled=1
-   gpgcheck=1
-   repo_gpgcheck=1
-   gpgkey=https://packages.cloud.google.com/yum/doc/yum-key.gpg https://packages.cloud.google.com/yum/doc/rpm-package-key.gpg
-   exclude=kubelet kubeadm kubectl
-EOF
-
-   # Set SELinux in permissive mode (effectively disabling it)
-   sudo setenforce 0
-   sudo sed -i 's/^SELINUX=enforcing$/SELINUX=permissive/' /etc/selinux/config
-
-   sudo yum install -y kubelet kubeadm kubectl --disableexcludes=kubernetes
-
-   sudo systemctl enable --now kubelet
-   systemctl stop firewalld
-   systemctl disable firewalld
-
-else
-
    sudo apt-get update && sudo apt-get install -y apt-transport-https curl
    curl -s https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo apt-key add -
    cat <<EOF | sudo tee /etc/apt/sources.list.d/kubernetes.list
